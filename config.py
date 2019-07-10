98519314240e45a0838f2ee08d09cf83c6feb2d5
Kris <sirk123au@gmail.com>
2019-07-10 14:07:50 +1000

    fixed paths

--- config.py
+++ config.py
@@ -6,11 +6,6 @@ from utils import YAMLBase
 
 class ConfigParser(YAMLBase):
     def __init__(self, filepath=None):
-        if not filepath:
-            # FIXME?
-            parent_dir = (os.path.abspath(os.path.join(
-                os.path.dirname(__file__), os.path.pardir)))
-            filepath = os.path.join(parent_dir, 'config.yml')
-
+        if not filepath: filepath = 'config.yml'
         super(ConfigParser, self).__init__(filepath)
 
