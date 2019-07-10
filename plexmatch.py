16c76a5d0d1fe7228f6dd7656a37b8f72bd82aff
Kris <sirk123au@gmail.com>
2019-07-10 20:03:25 +1000

    added stuff

--- plexmatch.py
+++ plexmatch.py
@@ -11,18 +11,19 @@ baseurl = config['plex']['baseurl']
 token = config['plex']['token']
 if os.name == 'nt': os.system('cls') 
 else: os.system('clear')
-try:
-  plex = PlexServer(baseurl, token)
-except:
-  raise Exception("No Plex server found at: {base_url}".format(base_url=baseurl))
+try: plex = PlexServer(baseurl, token)
+except: raise Exception("No Plex server found at: {base_url}".format(base_url=baseurl))
 print(r"Reading Plex Movie List", end='\r', flush=True)
 movies = plex.library.section('Movies')
+       
 for video in movies.all():
     fname = str(video.locations).strip('[]').split("/")[6]
+    with open('movies.txt', 'a') as f:
+      f.write("%s\n" % fname)
     for file in os.listdir(search_folder):
         if fnmatch.fnmatch(file, fname):
           #print (r"Matched {} ----> {}".format(fname , file), end='\r', flush=True)          
           match = True
           break
         else: match = False
-    if not match: print (r"Not Matched {}".format(fname),end='\r', flush=True)
+    if not match: print ("Not Matched {}".format(fname))
