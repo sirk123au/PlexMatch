d5b6811228e2a91c53247631edf9b4e99e2763c1
Kris <sirk123au@gmail.com>
2019-07-10 19:26:58 +1000

    Pretty up the code

--- plexmatch.py
+++ plexmatch.py
@@ -1,6 +1,7 @@
 PYTHONIOENCODING="UTF-8"  
 import fnmatch
 import os
+import time
 from config import ConfigParser
 from plexapi.server import PlexServer
 
@@ -8,22 +9,20 @@ config = ConfigParser()
 search_folder = config['plex']['search_folder']
 baseurl = config['plex']['baseurl']
 token = config['plex']['token']
-
+if os.name == 'nt': os.system('cls') 
+else: os.system('clear')
 try:
   plex = PlexServer(baseurl, token)
 except:
   raise Exception("No Plex server found at: {base_url}".format(base_url=baseurl))
-
+print(r"Reading Plex Movie List", end='\r', flush=True)
 movies = plex.library.section('Movies')
 for video in movies.all():
     fname = str(video.locations).strip('[]').split("/")[6]
-
     for file in os.listdir(search_folder):
         if fnmatch.fnmatch(file, fname):
-          #print ("\033[1;32;40m Matched {} ----> {}".format(fname , file))
+          #print (r"Matched {} ----> {}".format(fname , file), end='\r', flush=True)          
           match = True
           break
-        else:
-          match = False
-    if not match: print (u"Not Matched {}".format(fname))
-
+        else: match = False
+    if not match: print (r"Not Matched {}".format(fname),end='\r', flush=True)
