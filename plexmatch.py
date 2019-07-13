5186f4f8da2158c3bc835d5d727b25a337173277
Kris <sirk123au@gmail.com>
2019-07-14 09:32:04 +1000

    Fixed Foler Matching

--- plexmatch.py
+++ plexmatch.py
@@ -9,21 +9,27 @@ config = ConfigParser()
 search_folder = config['plex']['search_folder']
 baseurl = config['plex']['baseurl']
 token = config['plex']['token']
+
 if os.name == 'nt': os.system('cls') 
 else: os.system('clear')
 try: plex = PlexServer(baseurl, token)
 except: raise Exception("No Plex server found at: {base_url}".format(base_url=baseurl))
-print(r"Reading Plex Movie List", end='\r', flush=True)
-movies = plex.library.section('Movies')
-       
-for video in movies.all():
-    fname = str(video.locations).strip('[]').split("/")[6]
-    with open('movies.txt', 'a') as f:
-      f.write("%s\n" % fname)
-    for file in os.listdir(search_folder):
+
+print("Reading Plex Movie List")
+movies = plex.library.section('Movies').all()
+print("Found {} Movies on Plex".format(len(movies)))
+movie_local = os.listdir(search_folder)
+print("Found {} in Local Directory".format(len(movie_local)))
+
+for video in movies:
+    fname = video.locations[0].strip('[]').split("/")[4]
+    for file in movie_local:
         if fnmatch.fnmatch(file, fname):
-          #print (r"Matched {} ----> {}".format(fname , file), end='\r', flush=True)          
+          print (r"Matched {} ----> {}".format(fname , file))          
           match = True
           break
         else: match = False
-    if not match: print ("Not Matched {}".format(fname))
+    if not match: 
+      print ("Not Matched {}".format(fname.replace(':','')))
+else:
+  print("All Movies Match Plex")
